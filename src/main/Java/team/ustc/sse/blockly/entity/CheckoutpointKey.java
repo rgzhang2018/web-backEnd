9b76b656750ff435ac636da79256bf62e1ac47fb
rgzhang2018 <rgzhang@mail.ustc.edu.cn>
2019-05-25 16:20:58 +0800

    update db

---
+++ src/main/Java/team/ustc/sse/blockly/entity/CheckoutpointKey.java
@@ -0,0 +1,23 @@
+package team.ustc.sse.blockly.entity;
+
+public class CheckoutpointKey {
+    private Integer studentid;
+
+    private Integer checkpointid;
+
+    public Integer getStudentid() {
+        return studentid;
+    }
+
+    public void setStudentid(Integer studentid) {
+        this.studentid = studentid;
+    }
+
+    public Integer getCheckpointid() {
+        return checkpointid;
+    }
+
+    public void setCheckpointid(Integer checkpointid) {
+        this.checkpointid = checkpointid;
+    }
+}
---
+++ src/main/Java/team/ustc/sse/blockly/entity/FreedommodeKey.java
@@ -0,0 +1,23 @@
+package team.ustc.sse.blockly.entity;
+
+public class FreedommodeKey {
+    private Integer freemodeid;
+
+    private Integer studentid;
+
+    public Integer getFreemodeid() {
+        return freemodeid;
+    }
+
+    public void setFreemodeid(Integer freemodeid) {
+        this.freemodeid = freemodeid;
+    }
+
+    public Integer getStudentid() {
+        return studentid;
+    }
+
+    public void setStudentid(Integer studentid) {
+        this.studentid = studentid;
+    }
+}
